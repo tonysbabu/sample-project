f6477e9b1ec661a6def90b509ed9889f281e2ad9
Tony S Babu <tonysbabu@gmail.com>
2020-01-10 13:04:54 +0530

    added button

---
+++ src/Button.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+export default function Button() {
+    return (
+        <button type='button' style={{'width': '50px', height: '50px'}}>Click</button>
+    )
+    }    
